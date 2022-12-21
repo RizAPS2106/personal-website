d5f2e3e338aa22613dde8b44fbca64e4dd8c70ad
rizky <rizkyaditya2106@gmail.com>
2022-12-21 11:43:49 +0700

    adjusting

--- src/Components/Programming/Programming.js
+++ src/Components/Programming/Programming.js
@@ -5,47 +5,47 @@ import { PHP, Laravel, CodeIgniter, Go, Gin, Html, Css, Bootstrap, Javascript, J
 function Programming() {
   return (
     <div className='programming'>
-        <h1 className='title'>Programming Languages</h1>
-        <hr className='title-line' />
-        <div className='learn-languages'>
-          <div className='learn-language php current'>
-            <img src={PHP} className='img-language' alt='php' />
-            <ul className='framework-library'>
-              <li className='current'><img src={Laravel} className="img-framelib" alt='laravel' /> Laravel</li>
-              <li><img src={CodeIgniter} className="img-framelib" alt='codeigniter' /> CodeIgniter</li>
-            </ul>
-          </div>
-
-          <div className='learn-language html current'>
-            <img src={Html} className='img-language' alt='html' />
-          </div>
-
-          <div className='learn-language css current'>
-            <img src={Css} className='img-language' alt='css' />
-            <ul className='framework-library'>
-              <li className='current'><img src={Bootstrap} className="img-framelib" alt='bootstrap' /> Bootstrap</li>
-            </ul>
-          </div>
-
-          <div className='learn-language js current'>
-            <img src={Javascript} className='img-language' alt='php' />
-            <ul className='framework-library'>
-              <li><img src={Jquery} className="img-framelib" alt='jquery' /> Jquery</li>
-              <li className='current'><img src={ReactLogo} className="img-framelib" alt='react js' /> React</li>
-            </ul>
-          </div>
-          
-          <div className='learn-language go'>
-            <img src={Go} className='img-language' alt='golang' />
-            <ul className='framework-library'>
-              <li><img src={Gin} className="img-framelib" alt='gin' /> Gin</li>
-            </ul>
-          </div>
-
-          <div className='learn-language java'>
-            <img src={Java} className='img-language' alt='php' />
-          </div>
+      <h1 className='title'>Programming Languages</h1>
+      <hr className='title-line' />
+      <div className='learn-languages'>
+        <div className='learn-language php current'>
+          <img src={PHP} className='img-language' alt='php' />
+          <ul className='framework-library'>
+            <li className='current'><img src={Laravel} className="img-framelib" alt='laravel' /> Laravel</li>
+            <li><img src={CodeIgniter} className="img-framelib" alt='codeigniter' /> CodeIgniter</li>
+          </ul>
         </div>
+
+        <div className='learn-language html current'>
+          <img src={Html} className='img-language' alt='html' />
+        </div>
+
+        <div className='learn-language css current'>
+          <img src={Css} className='img-language' alt='css' />
+          <ul className='framework-library'>
+            <li className='current'><img src={Bootstrap} className="img-framelib" alt='bootstrap' /> Bootstrap</li>
+          </ul>
+        </div>
+
+        <div className='learn-language js current'>
+          <img src={Javascript} className='img-language' alt='php' />
+          <ul className='framework-library'>
+            <li><img src={Jquery} className="img-framelib" alt='jquery' /> JQuery</li>
+            <li className='current'><img src={ReactLogo} className="img-framelib" alt='react js' /> React</li>
+          </ul>
+        </div>
+
+        <div className='learn-language go'>
+          <img src={Go} className='img-language' alt='golang' />
+          <ul className='framework-library'>
+            <li><img src={Gin} className="img-framelib" alt='gin' /> Gin</li>
+          </ul>
+        </div>
+
+        <div className='learn-language java'>
+          <img src={Java} className='img-language' alt='php' />
+        </div>
+      </div>
     </div>
   )
 }
--- src/Containers/Header/Header.js
+++ src/Containers/Header/Header.js
@@ -8,7 +8,7 @@ function Header() {
         <img src={Rizaps} alt='R' className='word-art' />
         <div className='intro'>
             <h1 className='intro-title'>Rizky Aditya</h1>
-            <p className='intro-subtitle'>Entry lever coder</p>
+            <p className='intro-subtitle'>Entry level coder</p>
             <p className='intro-text'>
                 I'm currently into web programming. I still need to learn a lot on this large field. There are many things i want to learn and master. I still have a long way to go. More experience would be helpful.
             </p>
