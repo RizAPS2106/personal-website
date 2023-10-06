52201b8352299efd8fea6cfc933bfa43dbf98414
rizky <rizkyaditya2106@gmail.com>
2023-10-06 23:01:31 +0700

    change navbar item "coding" to "code"

--- src/Components/NavigationBar/NavigationBar.js
+++ src/Components/NavigationBar/NavigationBar.js
@@ -1,41 +1,41 @@
 import React, { useEffect } from 'react'
 import './NavigationBar.css'
-import {FaArrowUp} from 'react-icons/fa'
+import { FaArrowUp } from 'react-icons/fa'
 
 function NavigationBar() {
-  useEffect(() => {
-    window.onscroll = function() {
-        document.getElementById('topscroll').style.transition = "0.5s"
-        if (window.pageYOffset >= document.getElementById("navbar").offsetHeight) {
-            document.getElementById('topscroll').style.visibility = "visible"
-            document.getElementById('topscroll').style.opacity = "1"
-        } else {
-            document.getElementById('topscroll').style.visibility = "hidden"
-            document.getElementById('topscroll').style.opacity = "0"
-        }
-    };
-  })
-    
-  return (
-    <>
-        <nav id='navbar'>
-            <div className='logo'>
-                <div>
-                    <h1 className='logo-title'>Rizky Aditya</h1>
-                    <hr className='title-line' />
-                </div>
-            </div>
-            
-            <div className='nav-links'>
-                <a href='#statistic' className='nav-link'>Coding</a>
-                <a href='#certification' className='nav-link'>Certification</a>
-                <a href='#footer' className='nav-link'>Contact</a>
-            </div>
-        </nav>
+    useEffect(() => {
+        window.onscroll = function () {
+            document.getElementById('topscroll').style.transition = "0.5s"
+            if (window.pageYOffset >= document.getElementById("navbar").offsetHeight) {
+                document.getElementById('topscroll').style.visibility = "visible"
+                document.getElementById('topscroll').style.opacity = "1"
+            } else {
+                document.getElementById('topscroll').style.visibility = "hidden"
+                document.getElementById('topscroll').style.opacity = "0"
+            }
+        };
+    })
 
-        <div className='topscroll' id='topscroll' onClick={() => {window.scrollTo(0, 0)}}><FaArrowUp size={25} /></div> 
-    </>
-  )
+    return (
+        <>
+            <nav id='navbar'>
+                <div className='logo'>
+                    <div>
+                        <h1 className='logo-title'>Rizky Aditya</h1>
+                        <hr className='title-line' />
+                    </div>
+                </div>
+
+                <div className='nav-links'>
+                    <a href='#statistic' className='nav-link'>Code</a>
+                    <a href='#certification' className='nav-link'>Certification</a>
+                    <a href='#footer' className='nav-link'>Contact</a>
+                </div>
+            </nav>
+
+            <div className='topscroll' id='topscroll' onClick={() => { window.scrollTo(0, 0) }}><FaArrowUp size={25} /></div>
+        </>
+    )
 }
 
 export default NavigationBar
